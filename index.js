7570407f6c3f24370e5f4c1a827578a88d07b8f7
ArifRexa <arif.reza3126@gmail.com>
2021-10-29 15:20:47 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { MongoClient } = require('mongodb');
+const cors = require("cors");
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h6t6k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try{
+        await client.connect();
+        const database = client.db("theme_park");
+        const rideCollection = database.collection("rides");
+        // console.log("database connected");
+        
+
+        //Get Ride API
+        app.get("/rides", async(req, res) => {
+            const cursor = rideCollection.find({});
+            const rides = await cursor.toArray();
+            res.send(rides);
+        })
+
+        app.get("/check", async(req, res) => {
+            console.log("checked");
+            // const cursor = rideCollection.find({});
+            // const rides = await cursor.toArray();
+            res.send("this check is running");
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running too");
+})
+
+app.listen(port, () => {
+    console.log("listening from: ", port);
+})
---
+++ test.js
@@ -0,0 +1,178 @@
+// const test = [
+//     {
+//         rideName: "AFTERSHOCK",
+//         make: "Vekoma",
+//         height: "191 Feet",
+//         longestDrop: "177 Feet",
+//         topSpeed: "65.6 MPH",
+//         inversions: 3,
+//         verticalAngle: "90 Degrees",
+//         gForces: 4.5,
+//         duration: "1:32",
+//         opened: 2008,
+//         descriptions: "This metal monster dominates the skies over Silverwood. At 191 feet, this beast is really two thrills in one because not only does it take you forwards through a cobra roll and inverted loop, but backwards as well!Once you board, the cars will slowly move backwards up the starting tower until you’re staring straight at the ground. Then gravity will take over and pull you straight down as you reach speeds of up to 65 miles per hour! You’ll twist and turn though mind-blowing loops and rolls until you reach the second tower. This time, you’ll be looking at the sky as the coaster pulls you upward at a 90-degree angle, only to release and send you barreling through the whole thing backwards!",
+//         imgThumb:"https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/aftershock-1.jpg"
+//     },
+//     {
+//         rideName: "CORKSCREW",
+//         make: "Arrow Dynamics",
+//         height: "70 Feet",
+//         longestDrop: "1250 Feet",
+//         topSpeed: "46 MPH",
+//         inversions: 2,
+//         verticalAngle: "90 Degrees",
+//         gForces: 4.5,
+//         duration: "3:32",
+//         opened: 1990,
+//         descriptions: "This metal monster dominates the skies over Silverwood. At 191 feet, this beast is really two thrills in one because not only does it take you forwards through a cobra roll and inverted loop, but backwards as well!Once you board, the cars will slowly move backwards up the starting tower until you’re staring straight at the ground. Then gravity will take over and pull you straight down as you reach speeds of up to 65 miles per hour! You’ll twist and turn though mind-blowing loops and rolls until you reach the second tower. This time, you’ll be looking at the sky as the coaster pulls you upward at a 90-degree angle, only to release and send you barreling through the whole thing backwards!",
+//         imgThumb:"https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/corkscrew-1.jpg"
+//     },
+//     {
+//         rideName: "SPINCYCLE",
+//         make: "	VISA International",
+//         height: "105 Feet",
+//         longestDrop: "1250 Feet",
+//         topSpeed: "4 MPH",
+//         inversions: 6,
+//         verticalAngle: "170 Degrees",
+//         gForces: 3.5,
+//         duration: "2:32",
+//         opened: 2013,
+//         descriptions: "It’s the first of its kind in the world. A 104 foot tall thrill ride with a cylindrical-shaped vehicle that seats 24 people facing outward, with legs dangling and wearing only a harness. The vehicle rotates 360 degrees at 13 revolutions per minute while at the same time swings like a giant pendulum all the way upside down 104 feet above the ground bringing riders all the way around over and over again at three and a half times the normal gravitational pull. Will you be able to handle it?",
+//         imgThumb:"https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/spincycle-1.jpg"
+//     },
+//     {
+//         rideName: "PANIC PLUNGE",
+//         make: "A.R.M International",
+//         height: "150 Feet",
+//         longestDrop: "150 Feet",
+//         topSpeed: "47 MPH",
+//         inversions: 6,
+//         verticalAngle: "0 Degrees",
+//         gForces: 3.5,
+//         duration: "3:40",
+//         opened: 2006,
+//         descriptions: "It’s the first of its kind in the world. A 104 foot tall thrill ride with a cylindrical-shaped vehicle that seats 24 people facing outward, with legs dangling and wearing only a harness. The vehicle rotates 360 degrees at 13 revolutions per minute while at the same time swings like a giant pendulum all the way upside down 104 feet above the ground bringing riders all the way around over and over again at three and a half times the normal gravitational pull. Will you be able to handle it?",
+//         imgThumb:"https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/panic-2.jpg"
+//     },
+//     {
+//         rideName: "STUNT PILOT",
+//         make: "ROCKY International",
+//         height: "106 Feet",
+//         longestDrop: "150 Feet",
+//         topSpeed: "55 MPH",
+//         inversions: 4,
+//         verticalAngle: "120 Degrees",
+//         gForces: 3.5,
+//         duration: "3:40",
+//         opened: 2006,
+//         descriptions: "It’s the first of its kind in the world. A 104 foot tall thrill ride with a cylindrical-shaped vehicle that seats 24 people facing outward, with legs dangling and wearing only a harness. The vehicle rotates 360 degrees at 13 revolutions per minute while at the same time swings like a giant pendulum all the way upside down 104 feet above the ground bringing riders all the way around over and over again at three and a half times the normal gravitational pull. Will you be able to handle it?",
+//         imgThumb:"https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/stunt-pilot-3.jpg"
+//     },
+//     {
+//         rideName: "VELOCITY PEAK",
+//         make: "ROCKY International",
+//         height: "60 Feet",
+//         longestDrop: "60 Feet",
+//         topSpeed: "55 MPH",
+//         inversions: 1,
+//         verticalAngle: "120 Degrees",
+//         gForces: 3.5,
+//         duration: "3:00",
+//         opened: 2004,
+//         descriptions: "Our high speed water slide tower. If you've ever wanted to hit 55 mph on a thin layer of water in nothing but your swimsuit, these are your water slides. First you will climb up over 100 stairs and be faced with one of the toughest decisions of the day...which slide to go down first. Whether you choose 1, 2 or 3, you will definitely want to do all of them before you leave.",
+//         imgThumb:"https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/velocity-2.jpg"
+//     }
+
+// ]
+
+
+
+
+
+// [
+//     {
+//        "rideName": "AFTERSHOCK",
+//        "make": "Vekoma",
+//        "height": "191 Feet",
+//        "longestDrop": "177 Feet",
+//        "topSpeed": "65.6 MPH",
+//        "inversions": 3,
+//        "verticalAngle": "90 Degrees",
+//        "gForces": 4.5,
+//        "duration": "1:32",
+//        "opened": 2008,
+//        "descriptions": "This metal monster dominates the skies over Silverwood. At 191 feet, this beast is really two thrills in one because not only does it take you forwards through a cobra roll and inverted loop, but backwards as well!Once you board, the cars will slowly move backwards up the starting tower until you’re staring straight at the ground. Then gravity will take over and pull you straight down as you reach speeds of up to 65 miles per hour! You’ll twist and turn though mind-blowing loops and rolls until you reach the second tower. This time, you’ll be looking at the sky as the coaster pulls you upward at a 90-degree angle, only to release and send you barreling through the whole thing backwards!",
+//        "imgThumb": "https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/aftershock-1.jpg"
+//     },
+//     {
+//        "rideName": "CORKSCREW",
+//        "make": "Arrow Dynamics",
+//        "height": "70 Feet",
+//        "longestDrop": "1250 Feet",
+//        "topSpeed": "46 MPH",
+//        "inversions": 2,
+//        "verticalAngle": "90 Degrees",
+//        "gForces": 4.5,
+//        "duration": "3:32",
+//        "opened": 1990,
+//        "descriptions": "This metal monster dominates the skies over Silverwood. At 191 feet, this beast is really two thrills in one because not only does it take you forwards through a cobra roll and inverted loop, but backwards as well!Once you board, the cars will slowly move backwards up the starting tower until you’re staring straight at the ground. Then gravity will take over and pull you straight down as you reach speeds of up to 65 miles per hour! You’ll twist and turn though mind-blowing loops and rolls until you reach the second tower. This time, you’ll be looking at the sky as the coaster pulls you upward at a 90-degree angle, only to release and send you barreling through the whole thing backwards!",
+//        "imgThumb": "https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/corkscrew-1.jpg"
+//     },
+//     {
+//        "rideName": "SPINCYCLE",
+//        "make": "\tVISA International",
+//        "height": "105 Feet",
+//        "longestDrop": "1250 Feet",
+//        "topSpeed": "4 MPH",
+//        "inversions": 6,
+//        "verticalAngle": "170 Degrees",
+//        "gForces": 3.5,
+//        "duration": "2:32",
+//        "opened": 2013,
+//        "descriptions": "It’s the first of its kind in the world. A 104 foot tall thrill ride with a cylindrical-shaped vehicle that seats 24 people facing outward, with legs dangling and wearing only a harness. The vehicle rotates 360 degrees at 13 revolutions per minute while at the same time swings like a giant pendulum all the way upside down 104 feet above the ground bringing riders all the way around over and over again at three and a half times the normal gravitational pull. Will you be able to handle it?",
+//        "imgThumb": "https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/spincycle-1.jpg"
+//     },
+//     {
+//        "rideName": "PANIC PLUNGE",
+//        "make": "A.R.M International",
+//        "height": "150 Feet",
+//        "longestDrop": "150 Feet",
+//        "topSpeed": "47 MPH",
+//        "inversions": 6,
+//        "verticalAngle": "0 Degrees",
+//        "gForces": 3.5,
+//        "duration": "3:40",
+//        "opened": 2006,
+//        "descriptions": "It’s the first of its kind in the world. A 104 foot tall thrill ride with a cylindrical-shaped vehicle that seats 24 people facing outward, with legs dangling and wearing only a harness. The vehicle rotates 360 degrees at 13 revolutions per minute while at the same time swings like a giant pendulum all the way upside down 104 feet above the ground bringing riders all the way around over and over again at three and a half times the normal gravitational pull. Will you be able to handle it?",
+//        "imgThumb": "https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/panic-2.jpg"
+//     },
+//     {
+//        "rideName": "STUNT PILOT",
+//        "make": "ROCKY International",
+//        "height": "106 Feet",
+//        "longestDrop": "150 Feet",
+//        "topSpeed": "55 MPH",
+//        "inversions": 4,
+//        "verticalAngle": "120 Degrees",
+//        "gForces": 3.5,
+//        "duration": "3:40",
+//        "opened": 2006,
+//        "descriptions": "It’s the first of its kind in the world. A 104 foot tall thrill ride with a cylindrical-shaped vehicle that seats 24 people facing outward, with legs dangling and wearing only a harness. The vehicle rotates 360 degrees at 13 revolutions per minute while at the same time swings like a giant pendulum all the way upside down 104 feet above the ground bringing riders all the way around over and over again at three and a half times the normal gravitational pull. Will you be able to handle it?",
+//        "imgThumb": "https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/stunt-pilot-3.jpg"
+//     },
+//     {
+//        "rideName": "VELOCITY PEAK",
+//        "make": "ROCKY International",
+//        "height": "60 Feet",
+//        "longestDrop": "60 Feet",
+//        "topSpeed": "55 MPH",
+//        "inversions": 1,
+//        "verticalAngle": "120 Degrees",
+//        "gForces": 3.5,
+//        "duration": "3:00",
+//        "opened": 2004,
+//        "descriptions": "Our high speed water slide tower. If you've ever wanted to hit 55 mph on a thin layer of water in nothing but your swimsuit, these are your water slides. First you will climb up over 100 stairs and be faced with one of the toughest decisions of the day...which slide to go down first. Whether you choose 1, 2 or 3, you will definitely want to do all of them before you leave.",
+//        "imgThumb": "https://marvel-b1-cdn.bc0a.com/f00000000078081/www.silverwoodthemepark.com/rides/images/thumb/velocity-2.jpg"
+//     }
+//  ]
