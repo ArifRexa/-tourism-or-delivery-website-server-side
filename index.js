95d04d8bec20e158f9a668df3e8db2717d4d82c8
ArifRexa <arif.reza3126@gmail.com>
2021-10-30 11:59:57 +0600

    booking api delete feature

--- index.js
+++ index.js
@@ -62,6 +62,23 @@ async function run() {
             res.send(booking)
         })
 
+        //Bookings GET Single API
+
+        app.get("/bookings/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await ordersCollection.findOne(query);
+            res.json(result);
+        })
+
+        //Booking Delete API
+        app.delete("/bookings/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result)
+        })
+
         app.get("/check", async(req, res) => {
             console.log("checked");
             res.send("this check is running");
